78d61312a6756a09185ba80321c176c074e91b2d
taha <taha.sohail1999@gmail.com>
2020-07-16 03:13:31 +0500

    redux actions

--- src/Components/DishDetailComponent.js
+++ src/Components/DishDetailComponent.js
@@ -32,7 +32,7 @@ function RenderDish({ dish }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   if (comments != null)
     return (
       <div className="col-12 col-md-5 m-1">
@@ -54,7 +54,7 @@ function RenderComments({ comments }) {
             );
           })}
         </ul>
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment={addComment} />
       </div>
     );
   else return <div></div>;
@@ -79,7 +79,11 @@ const DishDetail = (props) => {
         <div className="row">
           <RenderDish dish={props.dish} />
 
-          <RenderComments comments={props.comments} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}
+          />
         </div>
       </div>
     );
@@ -106,8 +110,12 @@ export class CommentForm extends Component {
     this.setState({ isModalOpen: !this.state.isModalOpen });
   }
   handleSubmit(values) {
-    console.log("Your current state is:" + JSON.stringify(values));
-    alert("Your current state is:" + JSON.stringify(values));
+    this.props.addComment(
+      this.props.dishId,
+      values.rating,
+      values.author,
+      values.comment
+    );
   }
   render() {
     return (
